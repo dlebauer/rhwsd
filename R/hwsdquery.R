5903eaec2b4aeba4f16778e99b9e910c05b0da02
stineb <b.stocker@imperial.ac.uk>
2020-04-08 17:13:25 +0200

    removed obsolete functions

--- R/hwsdquery.R
+++ R/hwsdquery.R
@@ -1,31 +1,3 @@
-# ##' function to find a UTM zone
-# ##' \url{UTMzone}{http://en.wikipedia.org/wiki/Universal_Transverse_Mercator_coordinate_system#UTM_zone}
-# ##' @title long2UTM
-# ##' @param long longitude
-# ##' @return UTM Zone (integer)
-# ##' @export
-# ##' @author D G Rossiter
-# long2UTM <- function(long) {
-#   utmzone <- (floor((long + 180)/6) + 1)%%60 #floor(long + 180)/6) + 1) %% 60
-#   return(utmzone)
-# }
-# 
-# ##' function to create a box
-# ##'
-# ##' distinct (and simpler) from \code{rgeos::get.box}
-# ##' @title get box
-# ##' @param lat latitude
-# ##' @param lon longitude
-# ##' @param gridsize area around the point (size of Dx and Dy)
-# ##' @return abox is a vecotr of xmin, xmax, ymin, ymax
-# ##' @export
-# ##' @author David LeBauer
-# get.box <- function(lat, lon, gridsize = 0.1){
-#   tmp <- c(lat, lat, lon, lon) + gridsize/2*c(-1,1,-1,1)# * c(sign(lati)*c(-1, 1), sign(loni)*c(-1, 1))
-#   abox <- extent(tmp)
-#   return(abox)
-# }
-
 ##' Function to extract and format one rectangular window
 ##'
 ##' @title extract hwsd data from a region
@@ -78,60 +50,6 @@ get.hwsd <- function(x, con = con, hwsd.bil = NULL){
   return(result)
   
 }
-
-# ##' Function to extract and format one rectangular window
-# ##'
-# ##' @title extract hwsd data from a region defined by a box
-# ##' @param df_latlon A data frame with columns \code{lon} and \code{lat}.
-# ##' @param hwsd.bil (optional) location of file hwsd.bil. Too big for package repository. If necessary, this can be downloaded
-# ##' \url{here}{http://webarchive.iiasa.ac.at/Research/LUC/External-World-soil-database/HWSD_Data/HWSD_RASTER.zip}
-# ##' @return records queried from region
-# ##' @author D G Rossiter, David LeBauer
-# ##' @export
-# ##' @examples
-# ##' \dontrun{
-# ##' get.hwsd.latlon(lat = 44, lon = -80, gridsize = 0.1, con = con)
-# ##' }
-# # get.hwsd.latlon <- function(df_latlon, hwsd.bil = NULL, ...){
-# get.hwsd.latlon <- function(df, hwsd.bil = NULL, ...){
-#   result <- get.hwsd(x = df, con = con, hwsd.bil = hwsd.bil)
-#   return(result)
-# }
-
-# ##' Function to extract and format one rectangular window
-# ##'
-# ##' convenience wrapper for get.hwsd.latlon and get.hwsd.box
-# ##' @title extract hwsd data from a region
-# ##' @param abox (optional)
-# ##' @param lat (optional)
-# ##' @param lon (optional)
-# ##' @return records queried from region
-# ##' @author D G Rossiter, David LeBauer
-# ##' @export
-# ##' @examples
-# ##' \dontrun{
-# ##' lat <- 44; lon <- -80; gridsize <- 0.1
-# ##' abox<-c(lon, lon, lat, lat) + gridsize/2 * c(-1, 1, -1, 1)
-# ##' extract.one(abox)
-# ##' }
-# get.hwsd <- function(...){
-#   ## http://stackoverflow.com/a/19259158/199217
-#   .args <- as.list(match.call())[-1]
-#   print(names(.args))
-# 
-#   # if ("data.frame" %in% class(eval(.args[[1]]))) {
-#   #   hwsd.fn <- "get.hwsd.latlon"
-#   # } else {
-#   #   hwsd.fn <- "get.hwsd.box"
-#   # }
-# 
-#   hwsd.fn <- "get.hwsd.box"
-# 
-#   print(hwsd.fn)
-#   result <- do.call(hwsd.fn, .args)
-#   return(result)
-# }
-
 
 ##' Function to create connection to HWSD.sqlite database
 ##'
